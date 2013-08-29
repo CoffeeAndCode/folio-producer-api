804e9db45fd8b93bfce8904da30575e7dd605a53
Jonathan Knapp <jon@coffeeandcode.com>
2013-08-28 23:04:35 -0400

    started work on retrying failed API calls, brought in Promises for php

--- app/services/request.php
+++ app/services/request.php
@@ -2,6 +2,7 @@
 namespace DPSFolioProducer;
 
 class Request {
+    protected $is_retry = false;
     public $options = null;
     public $response_headers = array();
     public $response = null;
@@ -23,6 +24,18 @@ class Request {
                 user_error(json_last_error());
             }
         }
+
+        if ($this->get_response_code() === 200 &&
+            property_exists($this->response, 'status') &&
+            $this->response->status === 'InvalidTicket' &&
+            !$this->is_retry) {
+
+            // make create_session call
+                // on success, make original call
+                // $this->retry();
+                // on failure, fail method
+                // user_error('create_session call failed, unable to retry');
+        }
         return $this->response;
     }
 
@@ -31,9 +44,16 @@ class Request {
         if ($this->response_headers && count($this->response_headers)) {
             preg_match('/^(([a-zA-Z]+)\/([\d\.]+))\s([\d\.]+)\s(.*)$/', $this->response_headers[0], $matches);
             if ($matches) {
-                $response_code = $matches[4];
+                $response_code = intval($matches[4]);
             }
         }
         return $response_code;
+    }
+
+    public function retry() {
+        $this->response_headers = null;
+        $this->response = null;
+        $this->is_retry = true;
+        $this->run();
     }
 }
---
+++ test/PromisesTest.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class PromisesTest extends PHPUnit_Framework_TestCase {
+    public function test_deferred_has_promise_method() {
+        $deferred = new React\Promise\Deferred();
+        $this->assertTrue(method_exists($deferred, 'promise'));
+    }
+}
