dcd568152b696480285c778dc4f3a5a539f2365c
Jonathan Knapp <jon@coffeeandcode.com>
2013-08-30 08:06:11 -0400

    fixed gitignore issue that didn't allow committing Config.php class

---
+++ app/DPSFolioProducer/Config.php
@@ -0,0 +1,22 @@
+<?php
+namespace DPSFolioProducer;
+
+class Config {
+    public $data;
+
+    public function __construct($config=array()) {
+        $this->data = $config;
+    }
+
+    public function __get($name) {
+        return $this->data[$name];
+    }
+
+    public function __isset($name) {
+        return isset($this->data[$name]);
+    }
+
+    public function __set($name, $value) {
+        $this->data[$name] = $value;
+    }
+}
