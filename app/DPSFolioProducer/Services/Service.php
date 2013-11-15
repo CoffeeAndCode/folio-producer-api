d9af7c08ac0b837206125794620cc940fe204804
Jonathan Knapp <jon@coffeeandcode.com>
2013-11-15 17:08:12 -0500

    removed methods that are no longer needed in this class

--- app/DPSFolioProducer/Services/Service.php
+++ app/DPSFolioProducer/Services/Service.php
@@ -10,11 +10,6 @@ class Service
         return 'Authorization: AdobeAuth ticket="'.urlencode($this->config->ticket).'"';
     }
 
-    protected function auth_download_header()
-    {
-        return 'Authorization: AdobeAuth ticket="'.urlencode($this->config->download_ticket).'"';
-    }
-
     protected function create_url($suffix='')
     {
         $server = $this->config->api_server;
@@ -22,12 +17,6 @@ class Service
             $server = $this->config->request_server;
         }
 
-        return $server.'/webservices/'.$suffix;
-    }
-
-    protected function create_download_url($suffix='')
-    {
-        $server = $this->config->download_server;
         return $server.'/webservices/'.$suffix;
     }
 }
